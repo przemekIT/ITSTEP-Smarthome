923fae6514a84eb302c87c78f37c56b043627420
mmHare <madmarchhare65@gmail.com>
2025-10-11 22:32:16 +0200

    Django project

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
 """Main function to start the program"""
 
+"""
+Windows venv activate 
+python -m venv venv
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process
+.\venv\Scripts\Activate.ps1
+"""
+
+
 if __name__ == "__main__":
     pass
